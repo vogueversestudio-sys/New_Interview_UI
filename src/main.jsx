074a57de0f8cde138e4dbfa7705c567bf5ec76d8
vogueversestudio-sys <vogueversestudio-sys@users.noreply.github.com>
2026-02-09 12:00:30 +0530

    Fix React Router basename for GitHub Pages subdir routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/New_Interview_UI">
       <App />
     </BrowserRouter>
   </React.StrictMode>
